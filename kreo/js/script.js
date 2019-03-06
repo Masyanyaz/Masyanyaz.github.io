5fa92baff6a7c955e3f06406f57bf4e40d93fa1f
Masyanyaz <Masyanyaz@users.noreply.github.com>
2019-03-06 19:55:00 +0300

    11

--- kreo/js/script.js
+++ kreo/js/script.js
@@ -1,7 +1,12 @@
 $(function() {
 
-	$("a").click(function() {
-		alert("clicked");
-	});
+$('.anchor a').click(function(e) {
+        var anch = $(this);
+        $('html').animate({
+            scrollTop: $(anch.attr('href')).offset().top
+        }, 800);
+        e.preventDefault();
+        return false;
+    });
 
 });
