8c817adfa3815d59e7499683d7a4d21319d527fd
Masyanyaz <Masyanyaz@users.noreply.github.com>
2019-05-02 15:56:51 +0300

    1231

--- test/src/index.js
+++ test/src/index.js
@@ -5,12 +5,3 @@ import './js/'
 import './sass/main.sass'
 // CSS
 import './css/main.css'
-// Bootstrap
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// Jquery
-// import 'jquery/dist/jquery.min.js'
-
-
-// Bootstrap (example)
-// import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
-// import 'bootstrap/dist/css/bootstrap.min.css'
