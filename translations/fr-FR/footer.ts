cab2ba6cc1cea403b2d288d6ad8c1ab40c0b607f
Maksim <47759559+Masyanyaz@users.noreply.github.com>
2024-09-25 17:20:39 +0300

    New translations footer.ts (French)

---
+++ translations/fr-FR/footer.ts
@@ -0,0 +1,18 @@
+// @crowdin:ignore
+import type { TranslationsReference } from "@frontend/utils/i18n";
+// @crowdin:ignore
+import type { DataType as Reference } from "../en-001/footer";
+enum Data {
+  supportBanner_mainText = "Test fr",
+  supportBanner_secondaryText = "Hello fr",
+  supportBanner_buttonText = "Support fr",
+  appSection_main = `{brand} for {type, select,
+    ios {iOS}
+    android {Android}
+    windows {Windows}
+    other {{type}}
+  }`,
+}
+export default Data;
+export type DataType = typeof Data;
+(({} as DataType) satisfies TranslationsReference<Reference>);
