65378f131f2cae14c0f6bfb871b097b71cdceab5
Maksim <47759559+Masyanyaz@users.noreply.github.com>
2024-09-27 11:22:27 +0300

    New translations footer.[lucky].ts (Russian)

--- translations/ru-RU/footer.[lucky].ts
+++ translations/ru-RU/footer.[lucky].ts
@@ -1,9 +1,10 @@
-import type { TranslationsReference } from "../en-001/footer";
+import type { TranslationsReference } from "@frontend/utils/i18n";
 import type { DataType as Reference } from "../en-001/footer";
 enum Data {
-  supportBanner_mainText = "Test",
+  supportBanner_mainText = "Хорошо",
   supportBanner_secondaryText = "Приветике",
   supportBanner_buttonText = "Contact support",
+  test = "Это тест",
   appSection_main = `{brand} for {type, select,
     ios {iOS}
     android {Android}
